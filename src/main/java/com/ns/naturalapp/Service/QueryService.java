5eb2c5d71b1f777dbbb04193e302442d07559f54
skana1 <estera.skana01@gmail.com>
2023-03-07 11:39:40 +0100

    refresh Data

--- src/main/java/com/ns/naturalapp/Service/QueryService.java
+++ src/main/java/com/ns/naturalapp/Service/QueryService.java
@@ -2,46 +2,34 @@ package com.ns.naturalapp.Service;
 
 import com.ns.naturalapp.DTO.EntityDTO;
 import com.ns.naturalapp.DTO.Query;
-import com.ns.naturalapp.Config.Attribute;
-import com.ns.naturalapp.Config.Conditions;
-import com.ns.naturalapp.Config.View;
 import com.ns.naturalapp.DTO.QueryServiceInterface;
-import com.ns.naturalapp.Repository.AttributeRepo;
-import com.ns.naturalapp.Repository.ConditionsRepo;
+import com.ns.naturalapp.NaturalAppApplication;
 import com.ns.naturalapp.Repository.JDBCEntityRepository;
-import com.ns.naturalapp.Repository.ViewRepository;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
 
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
 public class QueryService implements QueryServiceInterface {
     private final String defaultLimit = "3";
     private final String defaultOffset = "0";
-    private final ViewRepository viewRepo;
-    private final AttributeRepo attributeRepo;
-    private final ConditionsRepo conditionsRepo;
     private final JDBCEntityRepository entityRepo;
 
-    public EntityDTO getView(String name, String limit, String offset, String[] requestConditions){
+    public EntityDTO getView(String name, String limit, String offset, String[] requestConditions) {
 
-        if(limit == null || limit == ""){
-             limit = defaultLimit;
+        if (limit == null || limit == "") {
+            limit = defaultLimit;
         }
-        if(offset == null || offset == ""){
+        if (offset == null || offset == "") {
             offset = defaultOffset;
         }
 
-            View view = viewRepo.findViewByName(name);
-            List<Attribute> attributes = attributeRepo.findAttributesByView(view);
-            Conditions condition = conditionsRepo.findConditionsByView(view);
-            Query query = new Query(view, attributes, condition, limit, offset,requestConditions);
-            return entityRepo.getEntity(query);
+        Query query = new Query(NaturalAppApplication.initMap.get(name).getView(), NaturalAppApplication.initMap.get(name).getAttributes(), NaturalAppApplication.initMap.get(name).getCondition(), limit, offset, requestConditions);
+        return entityRepo.getEntity(query);
     }
 
-    public String[] parseConditions(String requestConditions ){
-        if(requestConditions == "" || requestConditions == null){
+    public String[] parseConditions(String requestConditions) {
+        if (requestConditions == "" || requestConditions == null) {
             String[] empty = {};
             return empty;
         }
